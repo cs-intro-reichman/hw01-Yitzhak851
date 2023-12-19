75abf0c98c2f62ca058e32f36ad1467e4039f9ab
YBO <yitzhak851@gmail.com>
2023-12-19 12:09:33 +0200

    Adding Triangle file

--- week 1/Part 2/Triangle.java
+++ week 1/Part 2/Triangle.java
@@ -1,15 +1,21 @@
-// 4. Triangle
-// Three sides can form a triangle if the sum of the lengths of any two sides is greater than the
-// length of the remaining side. This is known as the Triangle Inequality Theorem. For example, the
-// three numbers 3, 4, 5 form a triangle, and the three numbers 2, 3, 6 don’t form a triangle. Write
-// a program (Triangle.java) that tests if three given integers form a triangle. Examples:
-// % java Triangle 3 4 5
-// 3, 4, 5: true
-// % java Triangle 2 3 6
-// 2, 3, 6: false
-
-
 
 public class Triangle {
-    
+    public static void main(String[] args) {
+        int x,y,z;
+        // getting the sides length
+        x = Integer.parseInt(args[0]);
+        y = Integer.parseInt(args[1]);
+        z = Integer.parseInt(args[2]);
+        boolean triangle = false;
+        // check the Triangle Inequality Theorem.
+        if ( (x+y > z) && (x+z > y) && (y+z > x) ){
+            triangle = true;
+        }
+
+        if (triangle) {
+            System.out.println( x + ", " + y + ", " + z + ": "+ triangle );
+        } else {
+            System.out.println( x + ", " + y + ", " + z + ": "+ triangle );
+        }
+    }
 }
