2e5a1074f0b6a8b8dfa640ccaf8c19abfdea13f9
YBO <yitzhak851@gmail.com>
2023-12-20 08:44:40 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -1,11 +1,10 @@
 public class Coins {
     public static void main(String[] args) {
-        int coins,cent, quarters, numOfQuarters;
+        int quarters,  coins, numOfQuarters,numOfCent ;
         quarters = 25;
-
         coins = Integer.parseInt(args[0]);
         numOfQuarters = coins / quarters ;
-        cent = coins % quarters;
-        System.out.println( "Use " + numOfQuarters + " quarters and " + cent + " cents");
+        numOfCent = coins % quarters;
+        System.out.println( "Use " + numOfQuarters + " quarters and " + numOfCent + " cents");
     }
 }
