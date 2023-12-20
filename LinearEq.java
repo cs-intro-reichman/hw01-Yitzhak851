61b1c2bfb32b80664df566766ba6e4729839e9e7
YBO <yitzhak851@gmail.com>
2023-12-20 18:33:04 +0200

    Update LinearEq.java
    Doing some re-factor to the
    LinearEq.java file

--- LinearEq.java
+++ LinearEq.java
@@ -1,11 +1,20 @@
+/**
+ * This class contain a "Triangle" mission.
+ * 
+ * @author (Yitzhak baror)
+ * @version (20.12.2023)
+ */
 public class LinearEq {
     public static void main(String[] args) {
-        double a,b,c, x;
-        a = Double.parseDouble(args[0]); // 1
-        b = Double.parseDouble(args[1]); // 2
-        c = Double.parseDouble(args[2]); // 1
-        x = (c-b)/a; // x= (1-2) / 1
-        System.out.println( a + " * x + " + b + " = " + c );
-        System.out.println("x = " + x );
+        // Declrate variables
+        double a, b, c, x;
+        // Getting value's from the user
+        a = Double.parseDouble(args[0]);
+        b = Double.parseDouble(args[1]);
+        c = Double.parseDouble(args[2]);
+        x = (c - b) / a;
+        // Print
+        System.out.println(a + " * x + " + b + " = " + c);
+        System.out.println("x = " + x);
     }
 }
