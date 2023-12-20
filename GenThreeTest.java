1e9a650dcf65dd25e066acb431cc4e5d4bbc6bd5
YBO <yitzhak851@gmail.com>
2023-12-20 14:31:03 +0200

    Update GenThreeTest.java

--- GenThreeTest.java
+++ GenThreeTest.java
@@ -1,25 +1,21 @@
 public class GenThreeTest {
     public static void main(String[] args) {
-        int maxNum, minNum, randomNum, count;
+        int maxNum, minNum, randomNum;
         int[] arr = new int[3];
         minNum = Integer.parseInt(args[0]);
         maxNum = Integer.parseInt(args[1]);
-        count = 0;
-        System.out.println("All tests passed!");
-        System.out.println("Actual:");
-        while (count < 3) {
+        for(int i=0; i<arr.length ; i++){
             randomNum = (int) (Math.random() * (maxNum - minNum)) + minNum;
             System.out.println(randomNum);
-            arr[count] = randomNum;
-            count++;
+            arr[i] = randomNum;
         }
-        // Find the x= minimum number
+        // Find x=minimum number
         int x = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] <= x){
                 x = arr[i];
             }
         }
-        System.out.print("The minimal generated number was " + x);
+        System.out.println("The minimal generated number was " + x);
     }
 }
