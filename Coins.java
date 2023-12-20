3f49e4ef163281d99f97c5ade9cb046732546788
YBO <yitzhak851@gmail.com>
2023-12-20 08:46:56 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -3,6 +3,9 @@ public class Coins {
         int quarters,  coins, numOfQuarters,numOfCent ;
         quarters = 25;
         coins = Integer.parseInt(args[0]);
+        if (coins==0) {
+            return;
+        }
         numOfQuarters = coins / quarters ;
         numOfCent = coins % quarters;
         System.out.println( "Use " + numOfQuarters + " quarters and " + numOfCent + " cents");
