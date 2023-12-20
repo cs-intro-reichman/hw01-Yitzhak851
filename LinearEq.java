6e82364af17f2c8e8378a99a37f287bde3bd0579
YBO <yitzhak851@gmail.com>
2023-12-20 09:23:44 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -1,13 +1,11 @@
 public class LinearEq {
     public static void main(String[] args) {
         double a,b,c, x;
-        // getting number of cents
         a = Double.parseDouble(args[0]); // 1
         b = Double.parseDouble(args[1]); // 2
         c = Double.parseDouble(args[2]); // 1
-        System.out.println( a + " * x + " + b + " = " + c );        
-        c = c-b; // c=-1
-        x = c/a; // x= -1/1
-        System.out.println("x = " + x );
+        x = (c-b)/a; // x= (1-2) / 1
+        System.out.printf("%.1f * x + %.1f = %.1f%n", a, b, c);
+        System.out.printf("X = %.1f%n", x);
     }
 }
