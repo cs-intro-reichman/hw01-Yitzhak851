7b777ea8ccab62050b2c1faa8617d51b3c770afb
YBO <yitzhak851@gmail.com>
2023-12-21 18:02:49 +0200

    Update GenThree.java
    Doing small re-factor

--- GenThree.java
+++ GenThree.java
@@ -1,26 +1,35 @@
 /**
  * This class contain a "GenThree" mission.
+ * 
  * @author (Yitzhak baror)
  * @version (20.12.2023)
  */
 public class GenThree {
     public static void main(String[] args) {
-        int maxNum, minNum, randomNum;
+        // Declrate variables
+        int x, y, maxNum, minNum, randomNum;
+        // Getting values from the user
+        x = Integer.parseInt(args[0]);
+        y = Integer.parseInt(args[1]);
+        // Calculate min && max number
+        minNum = Math.min(x, y);
+        maxNum = Math.max(x, y);
+        // Create array with the values of the randomNum*3
         int[] arr = new int[3];
-        minNum = Integer.parseInt(args[0]);
-        maxNum = Integer.parseInt(args[1]);
-        for(int i=0; i<arr.length ; i++){
+        // Loop that print the 3 random numbers
+        for (int i = 0; i < arr.length; i++) {
             randomNum = (int) (Math.random() * (maxNum - minNum)) + minNum;
             System.out.println(randomNum);
             arr[i] = randomNum;
         }
-        // Find x=minimum number
-        int x = arr[0];
+        // Calculate the min number from the 3 random num
+        int minGeneratedNum = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= x){
-                x = arr[i];
+            if (arr[i] <= minGeneratedNum) {
+                minGeneratedNum = arr[i];
             }
         }
+        // Print the result
         System.out.println("The minimal generated number was " + x);
     }
 }
