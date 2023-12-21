b3d3eb18b225b9acca256ff2655016127b079314
YBO <yitzhak851@gmail.com>
2023-12-21 17:51:00 +0200

    Update Coins File
    Adding API

--- Coins.java
+++ Coins.java
@@ -1,10 +1,20 @@
+/**
+ * This class contain a "Coins" mission.
+ * 
+ * @author (Yitzhak baror)
+ * @version (21.12.2023)
+ */
 public class Coins {
     public static void main(String[] args) {
-        int quarters,  coins, numOfQuarters,numOfCent ;
+        // Declrate variables
+        int quarters, coins, numOfQuarters, numOfCent;
         quarters = 25;
+        // Getting values from the user
         coins = Integer.parseInt(args[0]);
-        numOfQuarters = coins / quarters ;
+        // Calculate the result
+        numOfQuarters = coins / quarters;
         numOfCent = coins % quarters;
-        System.out.println( "Use " + numOfQuarters + " quarters and " + numOfCent + " cents");
+        // Print the result
+        System.out.println("Use " + numOfQuarters + " quarters and " + numOfCent + " cents");
     }
 }
