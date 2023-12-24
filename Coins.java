7a6d2e4d91a20c029242fcbef06b81a608226dd8
YBO <yitzhak851@gmail.com>
2023-12-24 22:27:23 +0200

    Update Coins.java
    doing some founction - cool re-factor

--- Coins.java
+++ Coins.java
@@ -5,6 +5,10 @@
  * @version (21.12.2023)
  */
 public class Coins {
+    public static int calcNumOfQuarters(int a, int b) {
+        int numOfQuarters = a / b;
+        return numOfQuarters;
+    }
     public static void main(String[] args) {
         // Declrate variables
         int quarters, coins, numOfQuarters, numOfCent;
@@ -12,7 +16,7 @@ public class Coins {
         // Getting values from the user
         coins = Integer.parseInt(args[0]);
         // Calculate the result
-        numOfQuarters = coins / quarters;
+        numOfQuarters = calcNumOfQuarters(coins, quarters);
         numOfCent = coins % quarters;
         // Print the result
         System.out.println("Use " + numOfQuarters + " quarters and " + numOfCent + " cents");
